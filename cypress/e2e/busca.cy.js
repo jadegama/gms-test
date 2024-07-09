4d57e5baea43abb178e4c4349e03778c774bb0dd
jadegama <jade_gama95@hotmail.com>
2024-07-08 22:13:52 -0300

    correção de teste e automação

--- cypress/e2e/busca.cy.js
+++ cypress/e2e/busca.cy.js
@@ -28,7 +28,7 @@ describe('US-001-Funcionalidade: Busca de filmes com palavra-chave valida', () =
 
     });
 
-    it.only('Deve buscar filme com sucesso de uma lista inteira', () => {
+    it('Deve buscar filme com sucesso de uma lista inteira', () => {
         cy.fixture('filmes').each((filmes) => {
             cy.get('#search-input').clear().type (filmes.titulo)
             cy.get('#search-button').click()
@@ -38,26 +38,22 @@ describe('US-001-Funcionalidade: Busca de filmes com palavra-chave valida', () =
     });
 
     it('Deve dar erro na busca por um filme que não está cadastrado', () => {
-      cy.visit('http://127.0.0.1:8080/')
       cy.get('#search-input').type ("meu malvado favorito")
       cy.get('#search-button').click()
       cy.get('#results-section > p').should('contain' , 'Filme não encontrado') 
    })
 
     it('Deve limpar a barra de busca quando o usuario solicitar', () => {
-     cy.visit('http://127.0.0.1:8080/')
      cy.get('#search-input').type ("matrix")
      cy.get('#clear-button').click ()
   })
 
   it('Mostrar as informações básicas da resposta da busca do usuário', () => {
-    cy.visit('http://127.0.0.1:8080/')
     cy.get('#search-input').type ("matrix")
     cy.get('#results-section')
    })
 
    it('Deve ter uma rolagem infinita', () => {
-    cy.visit('http://127.0.0.1:8080/')
     cy.get('#search-input').type ("matrix")
     cy.get('#results-section')
     
